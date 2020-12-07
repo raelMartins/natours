a2f6fc73ebb036bb8c2ff8f7e9edbfcd90bd0825
Martins Akeredolu <olamideakeredolu@gmail.com>
2020-12-07 13:21:17 +0100

    implemented cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,6 +25,17 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // MIDDLEWARE
+//Implement CORS
+app.use(cors());
+//api.natours.come client-side natours.com
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com'
+//   })
+// );
+
+app.options('*', cors());
+
 //Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 //set security http headers
